3046833defb752d2f056b8c32e2d6628baa23af7
gmyhp <rogerline0527@gmail.com>
2024-06-12 03:17:56 +0800

    Add the simulate.sh

---
+++ hw3/simulate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+flex scanner.l
+byacc -d -v parser.y
+g++ -o parser lex.yy.c y.tab.c -lfl
+./parser < ${1:-test.c}
+echo ""
+
+riscv32-unknown-elf-gcc ./Qemu/assembly/main.c codegen.S
+qemu-riscv32 a.out
